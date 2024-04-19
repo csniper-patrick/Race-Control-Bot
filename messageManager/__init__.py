f81286d26cef5db8c562983ba683d6859d71a6a1
Patrick Pun <patrickwypun04@gmail.com>
2024-04-19 11:16:04 +0100

    formatting

--- messageManager/__init__.py
+++ messageManager/__init__.py
@@ -5,20 +5,21 @@ import copy
 
 from .utils import *
 
+
 class messageManager:
-    
+
     from .RaceControlMessages import liveRaceControlMessagesHandler
     from .TimingDataF1 import liveTimingDataF1Handler
     from .TyreStintSeries import liveTyreStintSeriesHandler
     from .PitLaneTimeCollection import livePitLaneTimeCollectionHandler
-     
+
     def __init__(self, webhook, raceDirector="Race Director", tag=None, msgStyle={}):
         self.discord = Discord(url=webhook)
         if tag is not None:
-            self.tag=f" [{tag}]"
+            self.tag = f" [{tag}]"
         else:
-            self.tag=""
-        self.streamHandlers={
+            self.tag = ""
+        self.streamHandlers = {
             "TrackStatus": self.liveTrackStatusHandler,
             "RaceControlMessages": self.liveRaceControlMessagesHandler,
             "TimingAppData": self.liveTimingAppDataHandler,
@@ -26,61 +27,55 @@ class messageManager:
             "DriverList": self.liveDriverListHandler,
             "SessionInfo": self.liveSessionInfoHandler,
             "TyreStintSeries": self.liveTyreStintSeriesHandler,
-            "TimingDataF1" : self.liveTimingDataF1Handler,
-            "PitLaneTimeCollection": self.livePitLaneTimeCollectionHandler, 
+            "TimingDataF1": self.liveTimingDataF1Handler,
+            "PitLaneTimeCollection": self.livePitLaneTimeCollectionHandler,
         }
-        self.raceDirector=raceDirector
-        self.msgStyle={
+        self.raceDirector = raceDirector
+        self.msgStyle = {
             "flagColor": {
-				"GREEN": 5763719,
-				"CLEAR": 5763719,
-				"YELLOW": 16776960,
-				"DOUBLE YELLOW": 16776960, 
-				"CHEQUERED": 16777215,
-				"BLUE": 3447003,
-				"RED": 15548997	,
-				"BLACK AND WHITE": 16777215,
-				"BLACK AND ORANGE": 15105570,
-				"BLACK": 2303786
-        	},
-            "flagSymbol":{ 
-				"CHEQUERED": ":checkered_flag:",
-				"BLACK": ":flag_black:"
-			},
-        	"modeColor": {
-				"SAFETY CAR": 15844367,
-				"VIRTUAL SAFETY CAR": 15844367
-        	},
+                "GREEN": 5763719,
+                "CLEAR": 5763719,
+                "YELLOW": 16776960,
+                "DOUBLE YELLOW": 16776960,
+                "CHEQUERED": 16777215,
+                "BLUE": 3447003,
+                "RED": 15548997,
+                "BLACK AND WHITE": 16777215,
+                "BLACK AND ORANGE": 15105570,
+                "BLACK": 2303786,
+            },
+            "flagSymbol": {"CHEQUERED": ":checkered_flag:", "BLACK": ":flag_black:"},
+            "modeColor": {"SAFETY CAR": 15844367, "VIRTUAL SAFETY CAR": 15844367},
             "compoundColor": {
-				"SOFT": 15548997, # RED
-				"MEDIUM": 16776960, # YELLOW
-				"HARD": 16777215, # WHITE
-				"INTERMEDIATE": 2067276, # GREEN
-				"WET": 2123412, # BLUE
-        	},
-            "compoundSymbol": {}
-		}
+                "SOFT": 15548997,  # RED
+                "MEDIUM": 16776960,  # YELLOW
+                "HARD": 16777215,  # WHITE
+                "INTERMEDIATE": 2067276,  # GREEN
+                "WET": 2123412,  # BLUE
+            },
+            "compoundSymbol": {},
+        }
         self.msgStyle = updateDictDelta(self.msgStyle, msgStyle)
         return
-    
+
     def updateReference(self, msg):
         if "DriverList" in msg:
-            self.driverList=msg["DriverList"]
+            self.driverList = msg["DriverList"]
         if "SessionInfo" in msg:
-            self.sessionInfo=msg["SessionInfo"]
+            self.sessionInfo = msg["SessionInfo"]
         if "TimingStats" in msg:
-            self.timingStats=msg["TimingStats"]
+            self.timingStats = msg["TimingStats"]
         if "TimingAppData" in msg:
-            self.timingAppData=msg["TimingAppData"]
+            self.timingAppData = msg["TimingAppData"]
         if "TyreStintSeries" in msg:
-            self.tyreStintSeries=msg["TyreStintSeries"]
+            self.tyreStintSeries = msg["TyreStintSeries"]
         if "TimingDataF1" in msg:
-            self.timingDataF1=msg["TimingDataF1"]
-    
+            self.timingDataF1 = msg["TimingDataF1"]
+
     def pushDriverList(self):
         for number, info in self.driverList.items():
-            if type(info) is not dict: 
-                continue 
+            if type(info) is not dict:
+                continue
             self.discord.post(
                 username=f"{info['Tla']} - {info['RacingNumber']}",
                 embeds=[
@@ -89,32 +84,32 @@ class messageManager:
                             {"name": key, "value": info[key], "inline": True}
                             for key in ["FullName", "TeamName", "CountryCode"]
                         ],
-                        "color": int(info['TeamColour'], 16),
+                        "color": int(info["TeamColour"], 16),
                     }
                 ],
-                avatar_url=info["HeadshotUrl"] if "HeadshotUrl" in info else None
+                avatar_url=info["HeadshotUrl"] if "HeadshotUrl" in info else None,
             )
-            
+
         return
 
     def liveDriverListHandler(self, msg):
         driverList = msg["A"][1]
-        self.driverList=updateDictDelta(self.driverList, driverList)
+        self.driverList = updateDictDelta(self.driverList, driverList)
         return
-    
+
     def liveSessionInfoHandler(self, msg):
         sessionInfo = msg["A"][1]
-        self.sessionInfo=updateDictDelta(self.sessionInfo, sessionInfo)
+        self.sessionInfo = updateDictDelta(self.sessionInfo, sessionInfo)
         return
 
     def liveTrackStatusHandler(self, msg):
         return
-    
+
     def liveTimingStatsHandler(self, msg):
         self.timingStats = updateDictDelta(self.timingStats, msg["A"][1])
         return
 
     def liveTimingAppDataHandler(self, msg):
-        compoundColor=self.msgStyle["compoundColor"]
+        compoundColor = self.msgStyle["compoundColor"]
         self.timingAppData = updateDictDelta(self.timingAppData, msg["A"][1])
         return
--- messageManager/utils.py
+++ messageManager/utils.py
@@ -1,5 +1,6 @@
 import re
 
+
 def updateDictDelta(obj, delta):
     for key, value in delta.items():
         if key not in obj:
@@ -7,26 +8,31 @@ def updateDictDelta(obj, delta):
         elif type(value) == dict and type(obj[key]) == dict:
             obj[key] = updateDictDelta(obj[key], value)
         elif (
-            type(value) == dict and type(obj[key]) == list
-            and all([ k.isnumeric() for k in value.keys()])
+            type(value) == dict
+            and type(obj[key]) == list
+            and all([k.isnumeric() for k in value.keys()])
         ):
-            tempDict = dict(
-                [
-                    (str(idx), value)
-                    for idx, value in enumerate(obj[key])
-                ]
-            )
+            tempDict = dict([(str(idx), value) for idx, value in enumerate(obj[key])])
             tempDict = updateDictDelta(tempDict, value)
-            obj[key] = [
-                value
-                for _, value in tempDict.items()
-            ]
+            obj[key] = [value for _, value in tempDict.items()]
         else:
             obj[key] = value
     return obj
 
+
 def timeStr2msec(timeStr: str):
-    return sum([ val * scaler for val, scaler in zip( reversed([ float(i) for i in re.split(':', timeStr) ]), [1, 60] ) ]) * 1000
+    return (
+        sum(
+            [
+                val * scaler
+                for val, scaler in zip(
+                    reversed([float(i) for i in re.split(":", timeStr)]), [1, 60]
+                )
+            ]
+        )
+        * 1000
+    )
+
 
 def msec2timeStr(msec: int, signed: bool = False):
     val = int(abs(msec))
@@ -35,8 +41,8 @@ def msec2timeStr(msec: int, signed: bool = False):
     else:
         timeStr = "" if msec >= 0 else "-"
     if val >= 60000:
-        timeStr += str( val // 60000 ) + ":"
+        timeStr += str(val // 60000) + ":"
         val %= 60000
-    timeStr+= str(val // 1000)
-    timeStr+= "." + str(val % 1000).zfill(3)
+    timeStr += str(val // 1000)
+    timeStr += "." + str(val % 1000).zfill(3)
     return timeStr
